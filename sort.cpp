57029d63b8f0f10e3dfb3242bc81f92d505a4823
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-10-19 23:41:07 +0900

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -12,62 +12,62 @@ constexpr int PROC = 1<<5;
 constexpr int procp = 5;
 constexpr int NUM = 1 << 23;
 constexpr int nump = 23;
-
 constexpr int part = NUM / PROC;
 constexpr int partp = nump - procp;
+int myid, numproc, myp;
 int buf[NUM];
 
-void internal(int* a, int i, int j, int sz, int myid){
+void internal(int i, int j, int sz, int myid){
     int base = myid << partp;
     if(base & (sz - 1)) return;
     int d = 1 << (i - j);
     for(int k = base; k < base + sz; k++){
         int up = (((k >> i) & 2) == 0 ? 1 : 0);
         int idx = k - base;
-        if((k & d) == 0 && (a[idx] > a[idx | d]) == up){
-            int t = a[idx];
-            a[idx] = a[idx | d];
-            a[idx | d] = t;
+        if((k & d) == 0 && (buf[idx] > buf[idx | d]) == up){
+            int t = buf[idx];
+            buf[idx] = buf[idx | d];
+            buf[idx | d] = t;
         }
     }
 }
 
-int gather(int d, int myid, int* a){
+int gather(int d, int myid){
     int width = 1 << d;
     int num = width >> partp;
     int np = d - partp;
     if((myid & (num - 1)) == 0){
         int tmp = part;
         for(int i = 1; i < num; i++){
-            MPI_Recv(a + tmp, part, MPI_INT, myid + i,  myid + i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(buf + tmp, part, MPI_INT, myid + i,  myid + i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             tmp += part;
         }
     }
     else{
-        MPI_Send(a, part, MPI_INT, (myid >> np) << np, myid, MPI_COMM_WORLD);       
+        MPI_Send(buf, part, MPI_INT, (myid >> np) << np, myid, MPI_COMM_WORLD);       
     }
     return width;
 }
 
-void distribute(int myid, int width, int ij, int* a){
+void distribute(int width, int ij){
     int d = width >> 1; // next width
     int num = d >> partp; // num parts compose one set
-    if(((myid * part) & (width - 1)) == 0){
-        MPI_Send(a + d, d, MPI_INT, myid + num, myid, MPI_COMM_WORLD);
+    if((myp & (width - 1)) == 0){
+        MPI_Send(buf + d, d, MPI_INT, myid + num, myid, MPI_COMM_WORLD);
     }
-    else if((((myid + num) * part) & (width - 1)) == 0){
-        MPI_Recv(a, d, MPI_INT, myid - num, myid - num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    else if((((myid + num) << partp) & (width - 1)) == 0){
+        MPI_Recv(buf, d, MPI_INT, myid - num, myid - num, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 }
 
 int main(int argc, char** argv){
-    int myid, numproc;
     double t1, t2, elapsed;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numproc);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
+    myp = myid * part;
 
     if(myid == 0){
         for(int i = 0; i < NUM; i++) buf[i] = rand();
@@ -80,11 +80,11 @@ int main(int argc, char** argv){
     else std::sort(buf, buf + part);
 
     for(int i = partp; i < nump; i++){
-        int width = gather(i + 1, myid, buf);
+        int width = gather(i + 1, myid);
         for(int j = 0; j <= i; j++){
-            internal(buf, i, j, width, myid);
+            internal(i, j, width, myid);
             if(width > part){
-                distribute(myid, width, i - j, buf);
+                distribute(width, i - j);
                 width >>= 1;
             }
         }
