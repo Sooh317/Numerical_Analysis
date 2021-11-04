e6982f11fd6e2f83dbd723496d42c2dbd1ecd785
Sooh <63392428+Sooh317@users.noreply.github.com>
2021-11-04 12:20:19 +0900

    Update cuda.cu

--- NO2/cuda.cu
+++ NO2/cuda.cu
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 
 const int ITER = 100;
-const int S = 10;
+
+const long S = 1 << 12;
+const int BLOCK_NUM = 1024;
+const int THREAD_NUM = 256;
+const int widthp = 4; // 1 << widthp 個のブロックが正方形の1行に対応
 
 double elapsed(){
 	struct timespec ts;
@@ -14,40 +18,25 @@ double elapsed(){
 }
 
 __global__
-void kernel(int n, float r, float* a){
-    int NN = n*n;
-    float rr = 1 - 4*r;
-    for(int i = 0; i < ITER; i++){
-        float *dp = a + (i & 1)*NN, *ndp = a + (1 - (i & 1))*NN;
-	    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	    while(idx < NN){
-            bool dame = (idx - n < 0 || idx % n == 0 || idx % n == n - 1 || idx + n >= NN);
-            if(!dame) ndp[idx] = rr*dp[idx] + r*(dp[idx - n] + dp[idx + n] + dp[idx - 1] + dp[idx + 1]);
-		    idx += blockDim.x * gridDim.x;
-	    }
-        __syncthreads();
-    }
+void kernel(int i, long n, long NN, int wp, int height, float r, float rr, float* a){
+    float *dp = a + (i & 1)*NN, *ndp = a + (1 - (i & 1))*NN;
+	long base = n + 1;
+	long idx = base + (blockIdx.x >> wp) * n + (blockIdx.x & ((1 << wp) - 1)) * blockDim.x + threadIdx.x;
+	while(idx < NN - n){
+		ndp[idx] = rr*dp[idx] + r*(dp[idx - n] + dp[idx + n] + dp[idx - 1] + dp[idx + 1]);
+	    idx += n * height;
+	}
 }
-
-/*
-__global__
-void kernel(int N, float r, float rr, float *u){
-    int i = (threadIdx.x + 1) * 12;
-    for(int t=1; t<=100; t++){
-        int k = (t % 2) * N * N;
-        int kk = (1 - (t % 2)) * N * N;
-        for(int j=1; j<N-1; j++) u[k + i + j] = rr * u[kk + i + j] + r * (u[kk + i - N + j] + u[kk + i + j - 1] + u[kk + i + N + j] + u[kk + i + j + 1]);
-        __syncthreads();
-    } 
-}
-*/
 
 
 int main(){
-    const int N = S + 2;
+    const long N = S + 2;
+	const long NN = N*N;
+	const int height = BLOCK_NUM / (S / THREAD_NUM);
 	int size = (2*N*N) * sizeof(float);
 	float* dp_host = (float*)malloc(size);
     const float r = 0.1f;
+	const float rr = 1.0 - 4*r;
 
 	for(int i = 0; i < N*N; i++){
         if(i < N || i >= N*(N-1) || i % N == 0 || i % N == N - 1) continue;
@@ -62,7 +51,9 @@ int main(){
 
 	double t1 = elapsed();
 	
-	kernel<<<1,16>>>(N, r, dp_dev);
+	for(int i = 0; i < ITER; i++){
+		kernel<<<BLOCK_NUM,THREAD_NUM>>>(i, N, NN, widthp, height, r, rr, dp_dev);
+	}
 	cudaDeviceSynchronize();
 
 	double t2 = elapsed();
@@ -70,8 +61,9 @@ int main(){
 	cudaMemcpy(dp_host,dp_dev,size,cudaMemcpyDeviceToHost);
 
 	double t3 = elapsed();
-	for(int i = 0; i < N; i++){
-	    for(int j = 0; j < N; j++){
+
+	for(int i = 1; i <= 10; i++){
+	    for(int j = 1; j <= 10; j++){
             printf("%f ", dp_host[(ITER & 1)*(N*N) + i*N + j]);
         }
         printf("\n");
